03ccfe99f08f9680c7ff49f922e4ee7b738a421c
Nithin <94748016+Nithinlinga@users.noreply.github.com>
2025-02-22 07:49:23 +0000

    Add Flask /htop endpoint

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 import subprocess
-from flask import Flask
+from flask import Flask, send_from_directory
 from datetime import datetime
 import pytz
 import getpass
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
